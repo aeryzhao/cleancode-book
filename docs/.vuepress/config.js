1be835d4a0f04730bd2d676ad8b83d609a4dbd97
zhaoxg <iszhaoxg@outlook.com>
2021-05-14 09:02:08 +0800

    :rocket: (ci) config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   // 网站的标题
   title: "Clean Code 中文",
   // 上下文根
-  base: "/cleancode/",
+  base: "/cleancode-book/",
   themeConfig: {
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
     repo: "iszhaoxg/cleancode-book",
